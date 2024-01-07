b863753018b88c218380c9682ae3009cd84d7b2d
Verhille <titouan.verhille@epita.fr>
2024-01-07 20:49:38 +0100

    add pytest junixml support

---
+++ reports/pytest/test_capitalize.py
@@ -0,0 +1,11 @@
+def capital_case(x):
+    return x.capitalize()
+
+
+def test_capital_case():
+    assert capital_case("semaphore") == "Semaphore"
+
+
+def test_capital_case_error():
+    # error
+    assert capital_case("semaphore") == "semaphore"
